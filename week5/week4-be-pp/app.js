430cf285abb66c1d010c8529c2eefb61ad98cd3a
A Firose <afrin.techpm@gmail.com>
2025-11-18 12:05:08 +0200

    Add week5 project with proper structure

---
+++ week5/week4-be-pp/app.js
@@ -0,0 +1,16 @@
+// app.js
+const express = require('express');
+const app = express();
+
+// Import the router (instead of individual functions)
+const tourRouter = require('./routes/tourRouter.js');
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Use the router for all /tours routes
+app.use('/tours', tourRouter);
+
+// Start the server
+const port = 4000;
+app.listen(port, () => console.log(`Server on port ${port}`));
