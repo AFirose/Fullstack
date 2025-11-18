3886d15731bb76ec1da5848cdd43e58d54af5f3c
A Firose <afrin.techpm@gmail.com>
2025-11-18 10:25:28 +0200

    Reset repo and add only week5

--- week1/app.js
+++
@@ -1,6 +0,0 @@
-const myFunctions = require('./custom-functions');
-
-console.log("Cube of 3:", myFunctions.cube(3));
-console.log("Full name:", myFunctions.fullName("Afrin", "Firose"));
-console.log("2 to the power 4:", myFunctions.power(2, 4));
-console.log("Sum of cubes [1, 2, 3]:", myFunctions.sumCubes([1, 2, 3]));
--- week2/carArray/testCar.js
+++
@@ -1,4 +0,0 @@
-const Car = require('./carLib');
-
-console.log(Car.addOne("Fiesta", "Green", 1));
-console.log(Car.getAll());
